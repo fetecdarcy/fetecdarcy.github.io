b343182afa2650848b44b0d948dfe3860e158c84
FETEC <115225530+fetecdarcy@users.noreply.github.com>
2022-10-21 12:27:07 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
+document.body.style.setProperty('--background-color', '#f0f0f0');
+document.body.style.setProperty('--font-color', '#0f0f0f');
+
 const btnMobile = document.getElementById('btn-mobile');
+var btTheme = document.querySelector('.switch-toggle');
+var themeDark = false;
+
+
+btTheme.addEventListener('click', () => {
+    if(btTheme.checked) {
+        document.body.style.setProperty('--background-color', 'black');
+        document.body.style.setProperty('--font-color', '#f0f0f0');
+        themeDark = true;
+    }
+    else {
+        document.body.style.setProperty('--background-color', '#f0f0f0');
+        document.body.style.setProperty('--font-color', '#0f0f0f');
+        themeDark = false;
+    }
+    console.log(themeDark);
+});
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
