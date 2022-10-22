22fcfdbb39d0450f32aa440a96ba68b91b34c30d
RepolhoIguinorante <87026115+RepolhoIguinorante@users.noreply.github.com>
2022-10-22 00:32:18 -0300

    Coloquei o localStorage mais ainda nn testei
    Teste aí dps me fala

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 document.body.style.setProperty('--background-color', '#f0f0f0');
 document.body.style.setProperty('--font-color', '#0f0f0f');
 
+
 const btnMobile = document.getElementById('btn-mobile');
 var btTheme = document.querySelector('.switch-toggle');
 var themeDark = false;
 
+if(localStorage.bt) btTheme.checked = localStorage.bt;
 
 btTheme.addEventListener('click', () => {
     if(btTheme.checked) {
         document.body.style.setProperty('--background-color', 'black');
         document.body.style.setProperty('--font-color', '#f0f0f0');
         themeDark = true;
+        localStorage.bt = themeDark;
     }
     else {
         document.body.style.setProperty('--background-color', '#f0f0f0');
         document.body.style.setProperty('--font-color', '#0f0f0f');
         themeDark = false;
+        localStorage.bt = themeDark;
     }
     console.log(themeDark);
 });
