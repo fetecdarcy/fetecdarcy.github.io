cf4895e0a818ccc0dcd760ec4534e69d4e29c5c8
FETEC <115225530+fetecdarcy@users.noreply.github.com>
2022-10-24 22:45:29 -0300

    fetecdarcy

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 document.body.style.setProperty('--background-color', '#f0f0f0');
 document.body.style.setProperty('--background-color2', '#f0f0f0');
 document.body.style.setProperty('--font-color', '#0f0f0f');
+document.body.style.setProperty('--font-color2', '#0f0f0f');
 
 
 const btnMobile = document.getElementById('btn-mobile');
@@ -14,6 +15,7 @@ btTheme.addEventListener('click', () => {
         document.body.style.setProperty('--background-color', 'black');
         document.body.style.setProperty('--background-color2', 'black');
         document.body.style.setProperty('--font-color', '#f0f0f0');
+        document.body.style.setProperty('--font-color', 'black');
         themeDark = true;
         localStorage.bt = themeDark;
     }
